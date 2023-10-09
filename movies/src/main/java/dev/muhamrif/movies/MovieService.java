457a91aa08d7a80f2d265463d56740c0bc9138b3
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-10-09 12:34:08 -0400

    get movie by imdbid works

--- movies/src/main/java/dev/muhamrif/movies/MovieService.java
+++ movies/src/main/java/dev/muhamrif/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.muhamrif.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -11,6 +13,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
 
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
